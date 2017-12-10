b0f959b64bd2d57d12f5bce184188ec14872ef75
appleking10 <443562179@qq.com>
2017-12-11 00:39:58 +0800

    数据交互方面

--- js/index.js
+++ js/index.js
@@ -1,55 +1,117 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // if(isPC()){
     //     $('#a').text("pc");
     // }else{
     //     $('#a').text("moblie");
     // }
-    $('.sideBarShowBtn').click(function() {
+    $('.sideBarShowBtn').click(function () {
+        categoryAjax();
         $('.sideBar').show().addClass('animated fadeInLeft');
-        setTimeout(function(){
+        setTimeout(function () {
             $('.sideBar').removeClass('fadeInLeft');
         }, 1000);
         console.log('ok');
     });
-    $('.sideBarHideBtn').click(function() {
+    $('.sideBarHideBtn').click(function () {
         $('.sideBar').addClass('fadeOutLeft');
-        setTimeout(function(){
+        setTimeout(function () {
             $('.sideBar').removeClass('fadeOutLeft').hide();
-
-        }, 1000);    
+        }, 1000);
         console.log('ok');
     });
-    $('.searchBtn').click(function() {
-        let searchContnet = $('.searchInput').val();
-        // console.log(domain);
-        //拿到数据
-        if(searchContnet) {
-            $.ajax({
-                url: 'http://vtmer.cn/search',
-                dataType:'json'
-                }).done(function( data, status, xhr) {
-                    // console.log(status);
-                    if(status === "success") {
-                        // localStorage.setItem('search',searchContnet);
-                        // localStorage.setItem('SearchDate',data);
-                        $('.banner').addClass('animated fadeOutUp').hide();
-                        $('.searchWrapper').addClass('animated fadeInUpBig');
-                        setTimeout(function(){
-                            $('.banner').removeClass('fadeOutUp');
-                            $('.searchWrapper').removeClass('fadeInUpBig');
-                            
-                        }, 1000);    
-                        // history.go(0);
-                        // $('.homeWrapper').hide();
-                        addUrlPara('search', searchContnet);
-                    }
-                    
+
+    function loadResultPage(data) {
+        hideBanner('fadeInUpBig');
+        var bookLi = '';
+        var datas = data.bookclass[0].bookInfo;
+        var length = data.length;
+        for (var i = 0; i < length; i++) {
+            bookLi += "<li class='reslutBook'><div class='bookCoverBox'></div><div class='bookInfoBox'><div class='bookInfo'><span class='bookname'>" + datas[i].title + "</span>" + "<span class='author'>" + datas[i].author + "</span>" + "<span class='pulic'>" + datas[i].publish + "</span></div>" + "<span class='price'>参考价格：<a href=''>jingd</a>|<a href=''>dangdang</a>" + "</span></div></li>";
+        }
+        //    console.log(bookLi)
+        $('.reslutWrapper ul').html(bookLi);
+        $('.reslutWrapper').show();
+    }
+    function categoryAjax() {
+        $.ajax({
+            url: "http://book.evankoo.cn/category",
+            dataType: "json", //返回格式为json    
+            async: true, //请求是否异步，默认为异步，这也是ajax重要特性 
+            type: "GET", //请求方式    
+            beforeSend: function () {}, //请求前的处理
+            success: function (data) {
+
+                console.log(data);
+                var categoryLi = '';
+                for (var i = 0; i < data.data.length; i++) {
+                    categoryLi += '<li category-id=' + data.data[i].id + '>' + data.data[i].catalog + '</li>'
+                }
+                $('.sideBar .category').html(categoryLi);
+                $.each($('[category-id]'), function () {
+                    var ids = $(this).attr("category-id");
+                    $(this).on('click', function () {
+                        console.log(ids);
                     });
-        
-        }else {
+                });
+            }, //请求成功时处理    
+            complete: function () {}, //请求完成的处理    
+            error: function () {} //请求出错处理 
+        });
+    }
+
+    function bookAjax(ids) {
+        $.ajax({
+            url: "http://book.evankoo.cn/book",
+            dataType: "json", //返回格式为json    
+            data: {
+                'id': ids
+            },
+            async: true, //请求是否异步，默认为异步，这也是ajax重要特性 
+            type: "GET", //请求方式    
+            beforeSend: function () {}, //请求前的处理
+            success: function (data) {
+                console.log(data);
+            }, //请求成功时处理    
+            complete: function () {}, //请求完成的处理    
+            error: function () {} //请求出错处理 
+        });
+    }
+
+
+    // bookAjax()
+    //搜索接口
+    function searchAjax(searchContnet) {
+        $.ajax({
+            url: 'http://vtmer.cn/search',
+            dataType: 'json'
+        }).done(function (data, status, xhr) {
+            // console.log(data.bookclass[0].bookInfo);
+            if (status === "success") {
+                loadResultPage(data)
+            }
+        });
+    }
+    $('.searchBtn').click(function () {
+        let searchContnet = $('.searchInput').val();
+        //如果有搜索内容
+        if (searchContnet) {
+            searchAjax(searchContnet);
+            addUrlPara('search', searchContnet);
+        } else {
             alert("请输入关键词");
         }
+
     });
-    
-       
+
+    var ids = UrlSearch();
+    if (ids) {
+        hideBanner('fadeIn');
+        searchAjax(ids);
+
+    } else {
+        $('.banner').show();
+        $('.reslutWrapper').hide();
+    }
+
+
 });
--- js/load.js
+++ js/load.js
@@ -16,22 +16,52 @@
     }
     //向链接追加参数
     function addUrlPara(name, value) {  
-        var currentUrl = window.location.href.split('#')[0];  
+        var currentUrl = window.location.href.split('?')[0];  
+        console.log('1:'+currentUrl);
         if (/\?/g.test(currentUrl)) {  
             if (/name=[-\w]{4,25}/g.test(currentUrl)) {  
-                currentUrl = currentUrl.replace(/name=[-\w]{4,25}/g, name + "=" + value);  
+                currentUrl = currentUrl.replace(/name=[-\w]{4,25}/g, name + "=" + value); 
+                console.log('2:'+currentUrl); 
             } else {  
                 currentUrl += "&" + name + "=" + value;  
+                console.log('3:'+currentUrl);
             }  
         } else {  
             currentUrl += "?" + name + "=" + value;  
+            console.log('4:'+currentUrl);
         }  
-        if (window.location.href.split('#')[1]) {  
-            window.location.href = currentUrl + '#' + window.location.href.split('#')[1];  
-        } else {  
-            window.location.href = currentUrl;  
-        }  
+        // if (window.location.href.split('?')[1]) {  
+        //     window.location.href = currentUrl + '#' + window.location.href.split('?')[1];  
+        // } else {  
+        //     window.location.href = currentUrl;  
+        // }  
+        window.location.href = currentUrl; 
     } 
-
+// 隐藏banner
+    function hideBanner(animatedName){
+        $('.banner').addClass('animated fadeOutUp').hide();
+        $('.searchWrapper').addClass('animated '+animatedName);
+        setTimeout(function(){
+            $('.banner').removeClass('fadeOutUp');
+            $('.searchWrapper').removeClass(animatedName);
+        }, 1000);    
+    }
+    //获取id参数
+	function UrlSearch() {
+		var name, value;
+		var str = location.href; //获取到整个地址
+		var num = str.indexOf("?")
+		str = str.substr(num + 1); //取得num+1后所有参数，这里的num+1是下标 str.substr(start [, length ]
+		var arr = str.split("&"); //以&分割各个参数放到数组里
+		for (var i = 0; i < arr.length; i++) {
+			num = arr[i].indexOf("=");
+			if (num > 0) {
+				name = arr[i].substring(0, num);
+				value = arr[i].substr(num + 1);
+				this[name] = value;
+				return this[name];
+			}
+		}		
+    } 
 
   
